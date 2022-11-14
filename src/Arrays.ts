668d53ba188fd7e87b1a379ea8f64dec23c31cf6
Samuel MBote <36938470+SamuelMBote@users.noreply.github.com>
2022-11-14 18:22:29 -0300

    Configuração Inicial e Exercício de Arrays

---
+++ src/Arrays.ts
@@ -0,0 +1,31 @@
+interface ICurso {
+  nome: string;
+  horas: number;
+  aulas: number;
+  gratuito: false;
+  tags: string[];
+  idAulas: number[];
+  nivel: string;
+}
+export async function fetchCursos() {
+  const response = await fetch('https://api.origamid.dev/json/cursos.json');
+  const data = await response.json();
+  mostrarCursos(data);
+}
+
+export function mostrarCursos(cursos: ICurso[]) {
+  return (document.body.innerHTML +=
+    `<h2>Arrays</h2>` +
+    cursos.map((curso) => {
+      return `<div>
+    <h3 style="color:${curso.nivel == 'iniciante' ? 'blue' : 'red'}" >${
+        curso.nome
+      }</h3>
+      <p>Horas: ${curso.horas}</p>
+      <p>Aulas: ${curso.aulas}</p>
+      <p>Tipo: ${curso.gratuito ? 'Gratuito' : 'Pago'}</p>
+      <p>Tipo: ${curso.tags.join(', ').toLowerCase()}</p>
+      <p>Tipo: ${curso.idAulas.join(' | ')}</p>
+    </div>`;
+    }));
+}
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+import {fetchCursos} from './Arrays';
+fetchCursos();
