b27c8035b0186a288131f8be744cf508f4171a24
Samuel MBote <36938470+SamuelMBote@users.noreply.github.com>
2023-01-15 17:01:42 -0300

    Video Element

--- src/pratica/dom_files/classes/Slide.ts
+++ src/pratica/dom_files/classes/Slide.ts
@@ -20,7 +20,9 @@ export default class Slide {
     this.slides = slides;
     this.controls = controls;
     this.time = time;
-    this.index = 0;
+    this.index = localStorage.getItem('activeSlide')
+      ? Number(localStorage.getItem('activeSlide'))
+      : 0;
     this.slide = this.slides[this.index];
     this.timeout = null;
     this.paused = false;
@@ -30,22 +32,44 @@ export default class Slide {
   }
   hide(el: Element) {
     el.classList.remove('active');
+    if (el instanceof HTMLVideoElement) {
+      el.currentTime = 0;
+      el.pause();
+    }
   }
   show(index: number) {
     this.index = index;
     this.slide = this.slides[this.index];
+    localStorage.setItem('activeSlide', String(this.index));
+
     this.slides.forEach((el) => this.hide(el));
-    this.slides[index].classList.add('active');
-    this.auto(this.time);
+    this.slide.classList.add('active');
+    if (this.slide instanceof HTMLVideoElement) {
+      this.autoVideo(this.slide);
+    } else {
+      this.auto(this.time);
+    }
   }
   auto(time: number) {
     this.timeout?.clear();
     this.timeout = new Timeout(() => this.next(), time);
   }
+  autoVideo(video: HTMLVideoElement) {
+    video.muted = true;
+    video.play();
+    let firstPlay = true;
+    video.addEventListener('playing', () => {
+      if (firstPlay) this.auto(video.duration * 1000);
+      firstPlay = false;
+    });
+  }
   pause() {
     this.pausedTimeout = new Timeout(() => {
       this.timeout?.pause();
       this.paused = true;
+      if (this.slide instanceof HTMLVideoElement) {
+        this.slide.pause();
+      }
     }, 300);
   }
   continue() {
@@ -53,6 +77,9 @@ export default class Slide {
     if (this.paused) {
       this.paused = false;
       this.timeout?.continue();
+      if (this.slide instanceof HTMLVideoElement) {
+        this.slide.play();
+      }
     }
   }
   prev() {
